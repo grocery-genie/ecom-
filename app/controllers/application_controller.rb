8486499ba957b189c147c30788560eeabce4a2d9
Tyler Whitlow <tylerwhitlow@Tylers-MacBook-Air-2.local>
2019-01-24 14:45:17 -0500

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :categories, :brands
+
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+    Order.find(session[:order_id])
+  else
+    Order.new
+  end
+end
+
+
+
+  def categories
+    @categories = Category.order(:name)
+  end
+
+  def brands
+    @brands = Product.pluck(:brand).sort.uniq
+  end
+
+  protected
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+  end
+
+end 
